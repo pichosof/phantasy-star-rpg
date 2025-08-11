e6d86885af4d6fd55787d08b69d1c7ee98839ad1
Jonathan Mendonça <jonathan.mendonca@yahoo.com>
2025-08-10 22:48:06 -0300

    update tests

---
+++ tests/setup.ts
@@ -0,0 +1,12 @@
+import { beforeAll, beforeEach } from 'vitest';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import { db, schema } from '../src/infra/db';
+
+beforeAll(() => {
+  migrate(db, { migrationsFolder: 'drizzle/migrations' });
+});
+
+beforeEach(async () => {
+  await db.delete(schema.players);
+  await db.delete(schema.quests);
+});
